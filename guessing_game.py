ec8f32406fa2c91ce7f94ed9eed024955913bb80
TSIGANCHIK <ilya.mim.vop@gmail.com>
2025-10-01 21:58:14 +0300

    Removed unnecessary lambda - used a simple isdigit() check
    Improved variable naming:
    
    point → attempts (more descriptive name)
    
    otv → answer
    
    new_num → max_num
    
    num → user_input for input and num for the converted number
    
    Added input validation for the maximum number - so the user cannot enter an invalid value
    
    Simplified the attempt counting logic - now the counter always increments with valid input
    
    Removed redundant lines and improved formatting
    
    Simplified the game continuation logic - we only check if the answer is NOT "yes"
    
    Removed the aliased import - randint is a short enough name

--- guessing_game.py
+++ guessing_game.py
@@ -1,43 +1,51 @@
-from random import *
+from random import randint
 
 print('Welcome to Guessing Game!', "It's a good day to play", sep='\n')
 
-n = randint(1, 100)
-
-def is_valid(num):
-    return True if num.strip().isdigit() else False
-
-
 while True:
-    num = input('Введите число: ')
-    point = 0 #Number of attempts
+    attempts = 0  # Number of attempts
+
+    # Set maximum number
+    print('Do you want to set a maximum number?')
+    answer = input('Enter - Yes/No: ')
+
+    if answer.lower() == 'yes':
+        while True:
+            max_num = input('Enter maximum number: ')
+            if max_num.isdigit() and int(max_num) > 1:
+                n = randint(1, int(max_num))
+                break
+            else:
+                print('Please enter a number greater than 1')
+    else:
+        print('Hmm... Then let it be 100)')
+        n = randint(1, 100)
+
+    # Main game loop
     while True:
-        if is_valid(num):
-            num = int(num)
-        else:
-            print('', 'Упс, кажется это не число :)', sep='\n')
-            num = input('Введите число: ')
+        user_input = input('Enter a number: ')
+
+        # Check if input is a number
+        if not user_input.isdigit():
+            print('\nOops, it seems this is not a number :)')
             continue
 
+        num = int(user_input)
+        attempts += 1
+
+        # Check if user guessed the number
         if num < n:
-            print('Ваше число меньше  загаданного, попробуйте еще разок', end=' ')
-            num = input()
+            print('Your number is less than the target, try again')
         elif num > n:
-            print('Ваше число больше загаданного, попробуйте еще разок', end=' ')
-            num = input()
+            print('Your number is greater than the target, try again')
         else:
-            print('Вы угадали, поздравляем!')
-            print(f'Количество попыток: {point}')
+            print(f'You guessed it, congratulations!\nNumber of attempts: {attempts}')
             break
-    print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
-    print('Не желаете сыграть еще?')
-    otv = input()
-    if otv.lower() == 'да':
-        print('Хотите изменить максимально возможное значение?')
-        otv2 = input()
-        if otv2.lower() == 'да':
-            print('Введите максимально возможное значение', end=' ')
-            maxn = int(input())
-        continue
-    else:
+
+    # Ask user if they want to play again
+    print('Do you want to play again?')
+    answer = input('Enter - Yes/No: ')
+
+    if answer.lower() != 'yes':
+        print('Thank you for playing the number guessing game. See you again...')
         break
